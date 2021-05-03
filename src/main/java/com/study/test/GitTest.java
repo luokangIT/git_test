2aec3a879c92f16a594f831cf0bd6f39cf2bfc21
lk_glb <goodMoring_glb@local.com>
2021-05-03 20:24:54 +0800

    giteee push test commit

--- src/main/java/com/study/test/GitTest.java
+++ src/main/java/com/study/test/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println(111);
         System.out.println(333);
         System.out.println("push test");
+        System.out.println("gitee push test");
     }
 }
